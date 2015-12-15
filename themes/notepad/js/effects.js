110ec97ab390939417065ac97f2403041dee9124
Leslie-Alexandre DENIS <contact@ladenis.fr>
2015-12-15 20:34:54 +0100

    fluidvids.js

--- themes/notepad/js/effects.js
+++ themes/notepad/js/effects.js
@@ -5,4 +5,9 @@ $( document ).ready(function() {
   $( ".summer-page-container .summer-page-title h1" ).addClass( "animated lightSpeedIn" );
   $( ".fitvids" ).fitVids();
 
+  fluidvids.init({
+    selector: ['iframe'], // runs querySelectorAll()
+    players: ['www.youtube.com', 'player.vimeo.com'] // players to support
+  });
+
 });
