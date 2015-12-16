34010bec616ece9ac42af4b50e825caf14df5b8c
Leslie-Alexandre DENIS <contact@ladenis.fr>
2015-12-16 22:22:31 +0100

    fadeIn for pages images

--- themes/notepad/js/effects.js
+++ themes/notepad/js/effects.js
@@ -5,6 +5,7 @@ $(document).foundation();
 $( document ).ready(function() {
 
   $( ".summer-page-container .summer-page-title h1" ).addClass( "animated lightSpeedIn" );
+  $( ".summer-page-container .summer-page-content p.with-image" ).addClass( "animated fadeIn" );
   $( ".video iframe" ).wrap( '<div class="flex-video"></div>' );
 
   if (Modernizr.touch) {}
