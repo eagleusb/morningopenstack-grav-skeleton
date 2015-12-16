1e66cd87de9404613e1ebec1e9eb1de5f774ac5a
Leslie-Alexandre DENIS <contact@ladenis.fr>
2015-12-16 20:36:15 +0100

    effects.js flex-video wrapper

--- themes/notepad/js/effects.js
+++ themes/notepad/js/effects.js
@@ -1,12 +1,17 @@
 "use strict";
 
+$(document).foundation();
+
 $( document ).ready(function() {
 
   $( ".summer-page-container .summer-page-title h1" ).addClass( "animated lightSpeedIn" );
+  $( ".summer-post-content .post .video" ).wrap( '<div class="flex-video"></div>' );
 
   fluidvids.init({
     selector: ['iframe', 'object'], // runs querySelectorAll()
     players: ['www.youtube.com', 'player.vimeo.com'] // players to support
   });
+
+  if (Modernizr.touch) {}
 
 });
