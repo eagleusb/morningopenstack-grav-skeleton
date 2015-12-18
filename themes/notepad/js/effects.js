c1ab239033b0b584738f179cb33bce9d9fbd8f7b
Leslie-Alexandre DENIS <contact@ladenis.fr>
2015-12-18 23:00:42 +0100

    footer-carousel and particlesJS

--- themes/notepad/js/effects.js
+++ themes/notepad/js/effects.js
@@ -1,12 +1,30 @@
 "use strict";
 
-$(document).foundation();
+$( document ).foundation();
 
 $( document ).ready(function() {
 
   $( ".summer-page-container .summer-page-title h1" ).addClass( "animated lightSpeedIn" );
   $( ".summer-page-container .summer-page-content p.with-image" ).addClass( "animated fadeIn" );
+  $( ".summer-blog-menu ul li" ).hover(function() { $(this).addClass( "animated pulse" ); } );
   $( ".video iframe" ).wrap( '<div class="flex-video"></div>' );
+
+  $('.footer-carousel').slick({
+    autoplay: true,
+    autoplaySpeed: 2000,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    dots: false,
+    infinite: true,
+    fade: true,
+    cssEase: 'linear',
+    adaptiveHeight: false,
+    variableWidth: true
+  });
+
+  particlesJS.load('.summer-site-head-content', 'vendor/particles.json', function() {
+    console.log('callback - particles.js config loaded');
+  });
 
   if (Modernizr.touch) {}
 
